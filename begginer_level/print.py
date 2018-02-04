45fa18abd5e77ecf8b297f09f745ffd342179bf3
Ajjubhava <36122854+Ajjubhava@users.noreply.github.com>
2018-02-04 14:57:10 +0530

    Add files via upload

---
+++ begginer_level/print.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+for i in  range(1,21,+1):
+        print i
+for a in range(20,-1,-1):
+        print a
+for b in range(1,21,+2):
+        print b
+for c in range(2,21,+2):
+        print c
