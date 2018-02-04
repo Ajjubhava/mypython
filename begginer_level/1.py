d0938075fdea841818fb13cb5edef023cfd65da9
Ajjubhava <36122854+Ajjubhava@users.noreply.github.com>
2018-02-04 14:50:04 +0530

    Add files via upload

---
+++ begginer_level/1.py
@@ -0,0 +1,41 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#------------------------------------------------------------------------------
+#a=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+#print(a [0:20])
+#for a in xrange(1,21):
+ #   print a
+#for(i=20;i>=1;--i):
+ #   print i
+'''for i in  range(1,21,+1):
+        print i,
+for a in range(20,-1,-1):
+        print a,
+for b in range(1,21,+2):
+        print b,
+for c in range(2,21,+2):
+        print c
+a='programmer'
+print(a.count('r'))
+print (a.find('g'))
+print (a.replace('m','y'))'''
+x = int(input("Please enter an integer: "))
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
+
+
+
