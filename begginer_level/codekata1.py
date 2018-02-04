b9c02ff8d607183352e2d9fce83ac73b4567c0ee
Ajjubhava <36122854+Ajjubhava@users.noreply.github.com>
2018-02-04 16:06:01 +0530

    Add files via upload

---
+++ begginer_level/codekata1.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+
+a=input("enter a number")
+if a>0 and a<100000:
+    print"possitve value"
+elif a<0:
+    print "negative value"
+elif a==0:
+    print "It is zero"
+else:
+    print "invalid input"
