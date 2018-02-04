68c776cb8df584111e63d314c9a07d79a7f379fb
Ajjubhava <36122854+Ajjubhava@users.noreply.github.com>
2018-02-04 15:23:08 +0530

    Add files via upload

---
+++ begginer_level/assignment6.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a='programmer'
+print(a.count('r'))
+print (a.find('g'))
+print (a.replace('m','y'))
