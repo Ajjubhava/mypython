20bc48b12e82b33e000297d6939d0f719e8ac8c4
Ajjubhava <36122854+Ajjubhava@users.noreply.github.com>
2018-02-04 15:22:41 +0530

    Add files via upload

---
+++ begginer_level/assignment5.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+age=input('enter your age')
+if age==20:
+    print "eligible for voting"
+else:
+    print "not eligible"
